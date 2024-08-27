b6b97c7265fbe53ad156077dd49e14dc691d61c2
uriel <uriel@ubuntu.myguest.virtualbox.org>
2024-08-26 20:03:49 -0700

    Assignment 4 part 1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+//#define DEBUG_LOG(msg,...)
+#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
 #define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
 
 void* threadfunc(void* thread_param)
@@ -14,6 +14,22 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success = true;
+    DEBUG_LOG("Entering threadfunc");
+    
+    DEBUG_LOG("Waiting to mutex unlock for %d", thread_func_args->wait_to_obtain_ms);
+    usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    
+    DEBUG_LOG("Locking mutex for %d", thread_func_args->wait_to_obtain_ms);
+    pthread_mutex_lock(thread_func_args->mutex);
+    usleep(thread_func_args-> wait_to_release_ms* 1000);
+    
+    DEBUG_LOG("Unlocking mutex");
+    pthread_mutex_unlock(thread_func_args->mutex);
+    
+    DEBUG_LOG("Exiting threadfunc");
+
     return thread_param;
 }
 
@@ -28,6 +44,21 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    bool retVal = false;
+    int stat;
+    struct thread_data *threadData = (struct thread_data *)malloc(sizeof(struct thread_data));
+    threadData->mutex = mutex;
+    threadData->wait_to_obtain_ms = wait_to_obtain_ms;
+    threadData->wait_to_release_ms = wait_to_release_ms;
+
+    DEBUG_LOG("Entering start_thread_obtaining_mutex %d , %d",wait_to_obtain_ms, wait_to_release_ms);
+    stat = pthread_create(thread, NULL, threadfunc, (void* )threadData);
+    //pthread_join(thread, NULL);
+    if(stat == 0)
+    {
+        retVal = true;
+    }
+    DEBUG_LOG("Exiting start_thread_obtaining_mutex ");
+    return retVal;
 }
 
