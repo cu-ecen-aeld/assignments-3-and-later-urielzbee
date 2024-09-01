c9bd0f4768b9156d3d13367a9d5fd2b4bffbd87b
Uriel Zazueta <uriel.zazueta.barreras@gmail.com>
2024-09-01 14:09:45 -0700

    third Assigment 5 part 2

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -71,7 +71,7 @@ int main(int argc, char **argv)
 
     // Getting server info
 	memset(&hints, 0 ,sizeof(hints));
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
